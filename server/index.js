96e0f130b45cbf5c0ef4cc7442bd7764a72e1c36
CosmosMil <107584409+CosmosMil@users.noreply.github.com>
2023-04-27 14:57:00 +0200

    user config

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 const app = express();
 const port = process.env.PORT || 5001;
+import userRouter from "./routes/userRoutes.js";
 import cors from "cors";
 
 app.use(express.json());
@@ -10,6 +11,15 @@ app.use(
   })
 );
 app.use(cors());
+
 app.listen(port, () => {
   console.log("Server is running on port" + port);
 });
+
+app.use("/api/user", userRouter);
+
+// const helloFunction = (req, res) => {
+//   res.send("Heya!");
+// };
+
+// app.post("/test", helloFunction );
