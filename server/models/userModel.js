1cc389ba81dfbfb71fb4b77e4038ba388c649611
CosmosMil <107584409+CosmosMil@users.noreply.github.com>
2023-07-10 12:27:45 +0200

    changed NFT type back to array

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: true, unique: true },
     username: { type: String, required: true },
     password: { type: String, required: true },
-    NFTs: {type: Set, of: { type: mongoose.Schema.Types.ObjectId, ref: "nft" }},
+    NFTs: [{ type: mongoose.Schema.Types.ObjectId, ref: "nft" }],
     avatar: { type: String, default: "https://res.cloudinary.com/dte85mpkw/image/upload/v1683031892/users_profile_pics/placeholder_mehr42.jpg" },
     
     requests: [{type: mongoose.Schema.Types.ObjectId, ref: "swap"}],
